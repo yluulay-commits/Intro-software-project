4bd993c0e51678161201b29aafd7bcbd33ed2b80
yluulay-commits <yluulay@student.refactory.academy>
2026-01-11 08:29:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.loo Sog("Intro tftware Project");
+console.log("Terminal String Style & Logging Practice");
+
+console.log("\x1b[32m%s\x1b[0m", "SUCCESS: Project is running");
+console.log("\x1b[33m%s\x1b[0m", "WARNING: This is a demo");
+console.log("\x1b[31m%s\1b[0m", "ERROR: Sample error message");
